afdbc75ebbdfb296cdc507a08168cd131a8ef24f
John Carter <ma3a3@mail.ru>
2018-01-21 11:31:29 +0300

    new

--- На$$/Program.cs
+++ На$$/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace На__
 {
+    class Car
+    {
+        public readonly double MaxSpeed;
+        private double CurrentSpeed;
+        public Car()
+        {
+            MaxSpeed = 460;
+        }
+        public Car(double one)
+        {
+            MaxSpeed = one;
+        }
+        public double Speed
+        {
+            get { return CurrentSpeed; }
+            set
+            {
+                if (value > MaxSpeed)
+                {
+                    CurrentSpeed = MaxSpeed - 1;
+                }
+                else
+                {
+                    CurrentSpeed = value;
+                }
+            }
+        }
+    }
+
+    sealed class Offroad:Car
+    {
+        public void Test()
+        {
+            Console.WriteLine(Speed = 10);
+            
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Offroad Jeep = new Offroad();
+            Jeep.Speed = 328.192;
+            Console.WriteLine(Jeep.Speed);
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Фегуры/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фегуры
+{
+    class Fegura
+    {
+        protected float a, b, c, d;
+        public Fegura() { }
+        public Fegura(float a, float b, float c, float d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+    }
+
+    class Tring : Fegura
+    {
+        public Tring(float a, float b, float c) : base(a, b, c, 0) { }
+        public float Ploshad()
+        {
+            float half = (a + b + c) / 2;
+            return (float)Math.Sqrt(half * (half - a) * (half - b) * (half - c));
+        }
+    }
+
+    class Squar : Fegura
+    {
+        public Squar(float a, float b, float c, float d) : base(a, b, c, d) { }
+        public float Ploshad()
+        {
+            float half = (a + b + c + d) / 2;
+            return (float)Math.Sqrt((half - a) * (half - b) * (half - c) * (half - d));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Squar Kvadr = new Squar(12,2,3,12);
+            Console.WriteLine(Kvadr.Ploshad());
+            Console.ReadKey();
+        }
+    }
+}
